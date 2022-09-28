a60c10245f2798684c4022779c552563df66566e
Gabriel MP <contato.gabrielmartinspadoin@gmail.com>
2022-09-28 06:35:23 -0300

    [APP] SP002/0003 - feat: Criando a busca

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -5,6 +5,7 @@ import {
   RiShoppingCart2Line,
   RiShoppingCartFill
 } from 'react-icons/ri'
+import Busca from 'components/Busca';
 
 const iconeProps = {
   color: 'white',
@@ -31,7 +32,7 @@ function NavBar() {
         </div>
       </div>
       <div className={styles.busca}>
-
+        <Busca/>
       </div>
       <div className={styles.icones}>
         <a href="/carrinho">
