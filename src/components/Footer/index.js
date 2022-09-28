1c071666b193041ccefdb8c7aa5626984776db72
Gabriel MP <contato.gabrielmartinspadoin@gmail.com>
2022-09-28 07:46:28 -0300

    [APP] SP002/0004 - feat: Criando página padrão

---
+++ src/components/Footer/index.js
@@ -0,0 +1,28 @@
+import styles from './Footer.module.scss'
+import {
+  FaFacebook,
+  FaTwitter,
+  FaInstagram
+} from 'react-icons/fa';
+
+const iconeProps = {
+  color: 'white',
+  size: 24,
+}
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div>
+        <FaFacebook {...iconeProps}/>
+        <FaTwitter {...iconeProps}/>
+        <FaInstagram {...iconeProps}/>
+      </div>
+      <span>
+        Desenvolvido por Gabriel Martins Padoin
+      </span>
+    </footer>
+  )
+}
+
+export default Footer
